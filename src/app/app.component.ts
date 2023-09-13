5c07594a8e8400c10242ad470b856f8ee41961ac
BizarrIlliada <kazkoviy@icloud.com>
2023-09-14 01:14:39 +0300

    template-driven form is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, NgForm } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion: 'pet' | 'teacher' = 'pet';
+  questionAnswer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
+  // constructor(
+  //   private formBuilder: FormBuilder
+  // ) {}
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    this.signupForm.setValue({
+      userData: {
+        usernameInput: suggestedName,
+        emailInput: 'test@mail.com',
+      },
+      secretSelect: 'teacher',
+      questionAnswer: 'Olena',
+      gender: 'female',
+    })
+
+    // this.signupForm.form.patchValue({
+    //   userData: {
+    //     usernameInput: suggestedName,
+    //   }
+    // })
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm);
+
+      this.submitted = true;
+
+      this.user.username = this.signupForm.value.userData.usernameInput;
+      this.user.email = this.signupForm.value.userData.emailInput;
+      this.user.secretQuestion = this.signupForm.value.secretSelect;
+      this.user.answer = this.signupForm.value.questionAnswer;
+      this.user.gender = this.signupForm.value.gender;
+
+      this.signupForm.reset();
+    } else {
+      console.log('Form is invalid!');
+    }
   }
 }
